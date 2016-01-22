0a4176b0f3a0bbffaa4afe0828b5e132e574cab1
Bertalan Eva <eva0bertalan@gmail.com>
2016-01-22 10:16:12 +0100

    first version of first task

--- 1.js
+++ 1.js
@@ -1,5 +1,13 @@
 'use strict';
 
+function tripleEachElement(array) {
+  var tripletArray = [];
+  for(var i = 0; i < array.length; i++) {
+    tripletArray.push(array[i]*3);
+  }
+  return tripletArray
+}
+
 // Write a function that takes an array of numbers,
 // and returns a new array with the same length but all,
 // of it's elements are the triple of the original array's elements!
