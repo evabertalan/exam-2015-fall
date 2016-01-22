9031460447d9bb0c847e8e6615ba3f098d4110c7
Bertalan Eva <eva0bertalan@gmail.com>
2016-01-22 10:24:41 +0100

    first version of task 2

--- 2.js
+++ 2.js
@@ -1,5 +1,18 @@
 'use strict';
 
+function hasDivisableBy5(array) {
+  var dividedBy5 = 0;
+  for(var i = 0; i < array.length; i++) {
+    if(array[i] % 5 === 0) {
+      dividedBy5 += 1;
+    }
+  } if (dividedBy5 > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 // Write a function that checks, if an array contains a number
 // that has 5 as divisor. If it has it should return true otherwise false.
 
@@ -8,5 +21,3 @@
 
 console.log(hasDivisableBy5([1, 8, 3, 6, 2])); // false
 console.log(hasDivisableBy5([11, 7, 15, 9])); // true
-
-
