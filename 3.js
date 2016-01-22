3e1bc3b19fe924f1bdcc3643fa5df99a37686614
Bertalan Eva <eva0bertalan@gmail.com>
2016-01-22 10:47:53 +0100

    version 0 of task 3

--- 3.js
+++ 3.js
@@ -1,5 +1,22 @@
 'use strict';
 
+function Circle(radius) {
+  this.Circumference = 0;
+  this.area = 0;
+
+  this.getCircumference = function() {
+    this.Circumference = 2 * Math.PI * radius;
+    return this.Circumference;
+  }
+  this.getArea = function() {
+    this.area = Math.pow(radius, 2) * Math.PI;
+    return this.area
+  }
+  this.toString = function() {
+    return 'Radius: ' + radius + ', Circumference: ' + this.Circumference + ', Area: ' + this.area;
+  }
+}
+
 // Create a Circle constructor that creates a circle object:
 // it should take the circle's radius as a parameter
 // it should have a "getCircumference" method that returns it's circumference
@@ -8,11 +25,7 @@
 // 'Radius: 4, Circumference: 25.132741228718345, Area: 50.26548245743669'
 
 
-
-
-
-
-
-
-
-
+var circle = new Circle(4);
+console.log(circle.getCircumference());
+console.log(circle.getArea());
+console.log(circle.toString());
